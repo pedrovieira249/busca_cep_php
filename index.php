13524940d0d6fc1fb17ee26754ac1f9514becbdf
Pedro Vieira <angus.pedro@hotmail.com>
2020-11-26 17:31:45 -0300

    Calculo de cep

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			<div class="row">
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 					<div class="jumbotron mt-5">
-						<h1 class="display-4 mb-4">Informações de endereço por CEP</h1>
+						<h2 class="display-4 mb-4">Informações de endereço por CEP</h2>
 						<form action="index.php" method="post">
 							<div class="input-group">
 							 <input type="text" id="cep" class="cep form-control" name="cep" placeholder="Ex: 31910-660" required="required">
@@ -60,7 +60,7 @@
 			<div class="row">
 				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 					<div class="jumbotron mt-5">
-						<h1 class="display-4 mb-4">Informações de endereço por CEP - Via Ajax</h1>
+						<h2 class="display-4 mb-4">Informações de endereço por CEP - Via Ajax</h2>
 						<div class="input-group">
 							<input type="text" id="cepajax" class="cep form-control" name="cepajax" placeholder="Ex: 31910-660" required="required">
 							<div class="input-group-prepend">
@@ -90,6 +90,131 @@
 				</div>
 			</div>
 		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<form action="index.php" method="post">
+						<div class="jumbotron mt-5">
+							<h2 class="display-4 mb-5">Calcular preços, prazos e tipo de entrega</h2>
+							<div class="row">
+								<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+									<div class="form-group">
+										<label for="cep_ori">Cep de Origem</label>
+										<input type="cep_ori" class="cep form-control" id="cep_ori" name="cep_ori" aria-describedby="Cep de Origem" placeholder="">
+									</div>
+									<div class="form-group">
+										<label for="cep_dest">Cep de Destino</label>
+										<input type="cep_dest" class="cep form-control" id="cep_dest" name="cep_dest" aria-describedby="Cep de Destino" placeholder="">
+									</div>
+								</div>
+								<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+									<div class="form-group">
+										<label for="servico">Serviços de postagem</label>
+										<select name="servico" class="form-control" >
+											<!-- <option value="40215">SEDEX10</option>
+											<option value="40045">SEDEXACOBRAR</option> -->
+											<option value="40010">SEDEX</option>
+											<option value="41106">PAC</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+									<div class="form-group">
+										<label for="">Serviços adicionais</label><br>
+										<input type="checkbox" id="MaoPropria" name="MaoPropria" value="MaoPropria">
+										<label for="MaoPropria">Mão própria</label><br>
+
+										<input type="checkbox" id="avisoRecebimento" name="avisoRecebimento" value="avisoRecebimento">
+										<label for="avisoRecebimento">Aviso de recebimento - AR</label><br>
+
+										<input type="checkbox" id="ckValorDeclarado" name="ckValorDeclarado" value="ckValorDeclarado" onchange="mostraValorDeclarado(this.value);">
+										<label for="ckValorDeclarado">Declaração de Valor</label><br><br>
+										<script type="text/javascript">
+											function mostraValorDeclarado(val){
+												var valid = document.getElementById('ckValorDeclarado').checked;
+												var valorDeclarado = document.getElementById('valorDeclarado');
+												if(val == 'ckValorDeclarado' && valid){
+													valorDeclarado.style.display = "block";
+												}else if(val == 'ckValorDeclarado' && !valid){
+													valorDeclarado.style.display = "none";
+												}
+											}
+										</script>
+									</div>
+								</div>
+								<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
+									<div class="form-group">
+										<label for="peso">Peso <span class="col-form-label-sm"><b>Em gramas (caixa + produto)</b></span></label>
+										<input type="peso" class="form-control" id="peso" name="peso" aria-describedby="peso" placeholder="Ex: 0.5">
+									</div>
+									<div class="form-group" id="valorDeclarado" style="display:none;">
+										<label for="valorDeclarado">Valor Declarado</label>
+										<input type="valorDeclarado" class="form-control" id="valorDeclarado" name="valorDeclarado" aria-describedby="valorDeclarado">
+									</div>
+								</div>
+								<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+									<div class="row">
+										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+											<div class="form-group">
+												<label for="altura">Altura</label>
+												<input type="altura" class="form-control" id="altura"  name="altura" aria-describedby="altura">
+											</div>
+										</div>
+										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+											<div class="form-group">
+												<label for="lagura">Largura</label>
+												<input type="lagura" class="form-control" id="lagura"  name="lagura" aria-describedby="lagura">
+											</div>
+										</div>
+										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+											<div class="form-group">
+												<label for="comprimento">Comprimento</label>
+												<input type="comprimento" class="form-control" id="comprimento"  name="comprimento" aria-describedby="comprimento">
+											</div>
+										</div>
+										<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+											<div class="form-group">
+												<button class="btn btn-success d-block mx-auto">Calcular Frete</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+					<?php
+					$cep_origem = trim($_POST['cep_ori']);
+					$cep_destino = trim($_POST['cep_dest']);
+					$peso = trim($_POST['peso']);
+					$comprimento = trim($_POST['comprimento']);
+					$altura = trim($_POST['altura']);
+					$largura = trim($_POST['lagura']);
+					$valor_declarado = '0';
+					$cod_servico = trim($_POST['servico']);
+					// if( $cod_servico == '40215' ) $cod_servico_name = 'SEDEX10'; 
+					// if( $cod_servico == '40045' ) $cod_servico_name = 'SEDEXACOBRAR' ; 
+					if( $cod_servico == '40010' ) $cod_servico_name = 'SEDEX'; 
+					if( $cod_servico == '41106' ) $cod_servico_name = 'PAC' ;
+					$correios = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCepOrigem=".$cep_origem."&sCepDestino=".$cep_destino."&nVlPeso=".$peso."&nCdFormato=1&nVlComprimento=".$comprimento."&nVlAltura=".$altura."&nVlLargura=".$largura."&sCdMaoPropria=n&nVlValorDeclarado=".$valor_declarado."&sCdAvisoRecebimento=n&nCdServico=".$cod_servico."&nVlDiametro=0&StrRetorno=xml";
+					$xml = simplexml_load_file($correios);
+					if($xml->cServico->Erro == '0'){ ?>
+						<ul class="list-group">
+							<li class="list-group-item list-group-item-dark">Seviço: <?php echo $cod_servico_name;?></li>
+							<li class="list-group-item list-group-item-dark">Valor: <?php echo $xml->cServico->Valor;?></li>
+							<li class="list-group-item list-group-item-dark">Prazo de Entrega: <?php echo $xml->cServico->PrazoEntrega;?> dia(s)</li>
+						</ul>
+			  <?php }else{
+					echo "<br><br><div class='alert alert-danger'>Não foi possivel calcular a entrega</div><br>";
+					} ?>
+				</div>
+			</div>
+		</div><br><br><br>
 	</body>
 	<script type="text/javascript" src="_js/jquery.min.js"></script>
 	<script type="text/javascript" src="_js/popper.min.js"></script>
